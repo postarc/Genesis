4329a5a07ec768c5188343719bf44265fd61df37
postarc <45208608+postarc@users.noreply.github.com>
2019-08-31 20:23:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ GENESIS_BRANCH="master"
 DEFAULT_PORT=7233
 ERR=$(dpkg-query -l lsof | grep Err)
 clear
-if [ -z $ERR ]; then apt install -y lsof >/dev/null 2>&1 ; fi 
+if [ -z "$ERR" ]; then apt install -y lsof >/dev/null 2>&1 ; fi 
 while [ -n "$(sudo lsof -i -s TCP:LISTEN -P -n | grep $DEFAULT_PORT)" ]
 do
 ((DEFAULT_PORT++))
